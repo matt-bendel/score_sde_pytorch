4e2479a92a2dd1fc93039fdf6bbaac6e4aacf59b
Matt Bendel <bendel.8@buckeyemail.osu.edu>
2022-10-28 23:11:07 -0400

    Testing

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file(
   "config", None, "Training configuration.", lock_config=True)
 flags.DEFINE_string("workdir", None, "Work directory.")
-flags.DEFINE_enum("mode", None, ["train", "eval"], "Running mode: train or eval")
+flags.DEFINE_enum("mode", None, ["train", "eval", "inpaint"], "Running mode: train or eval")
 flags.DEFINE_string("eval_folder", "eval",
                     "The folder name for storing evaluation results")
 flags.mark_flags_as_required(["workdir", "config", "mode"])
